9046177c50d8e419d3909642ec647e72485891cd
Ben <ben3000@gmx.de>
2017-02-02 12:51:55 +0000

    final

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
+
+   var returnObj = {};
+   returnObj.ipadress = req.headers['x-forwarded-for']
+   returnObj.language = req.headers["accept-language"].slice(0, 2);
+   returnObj.software = req.headers['user-agent']
    
-   res.send("Hello, world") 
+   res.send(returnObj) 
 })
 
 app.listen(8080, function() {
